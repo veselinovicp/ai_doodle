51d943b15764fd9e6bd51e40262f38b3851185fd
dkegle <domenkeglevic@gmail.com>
2018-02-12 19:13:07 +0100

    Basic layout

--- web_app.py
+++ web_app.py
@@ -3,12 +3,19 @@ from flask import Flask, render_template
 
 
 # initialize flask app
-app = Flask(__name__, template_folder='webapp_templates')
+app = Flask(__name__)
 
+
+# main page
 @app.route('/')
 def index():
 	return render_template('index.html')
 
+
+# user clicked stylize
+@app.route('/stylize/', methods=['POST'])
+def style():
+	return img
 
 
 if __name__ == "__main__":
